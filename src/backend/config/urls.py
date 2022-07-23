ebd343106e4a6c0c968caeb41ae020c418da32ad
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-07-24 00:02:59 +0300

    Fix urls

--- src/backend/config/urls.py
+++ src/backend/config/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
