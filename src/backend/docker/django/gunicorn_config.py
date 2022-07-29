8c7af05a83995deadacf631d7920f89a66949c4a
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-07-29 19:55:48 +0300

    Add gunicorn reload (For a while) and fix mypy config

--- src/backend/docker/django/gunicorn_config.py
+++ src/backend/docker/django/gunicorn_config.py
@@ -8,6 +8,7 @@ bind = '0.0.0.0:8000'
 # Concerning `workers` setting see:
 # https://github.com/wemake-services/wemake-django-template/issues/1022
 workers = multiprocessing.cpu_count() * 2 + 1
+reload = True
 
 max_requests = 2000
 max_requests_jitter = 400
