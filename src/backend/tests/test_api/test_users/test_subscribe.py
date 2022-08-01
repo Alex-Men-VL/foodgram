266fb74fd0a47af0934de4cf78dbe38fe1589b50
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-01 20:08:11 +0300

    Add test for user delete subscribe endpoint

--- src/backend/tests/test_api/test_users/test_subscribe.py
+++ src/backend/tests/test_api/test_users/test_subscribe.py
@@ -9,6 +9,7 @@ from apps.users.api.serializers import UserSubscriptionSerializer
 from apps.users.models import CustomUser
 
 from ...factories import login_user
+from ...factories import SubscriptionFactory
 from ...factories import UserFactory
 
 
@@ -23,10 +24,11 @@ class UserViewSetSubscribeTest(
         self.author: CustomUser = UserFactory()
         self.base_url = 'api:users-subscribe'
 
-    def test_successful_subscribe_with_status_201(self) -> None:
+    def test_user_can_subscribe_on_other_user(self) -> None:
         """Проверка успешного оформления подписки"""
 
         login_user(self.client, self.user)
+
         response = self.client.get(
             reverse(self.base_url, args=[self.author.pk]),
         )
@@ -42,6 +44,7 @@ class UserViewSetSubscribeTest(
             context={'request': response},
         )
         self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.data['is_subscribed'], True)
 
         subscriptions = Subscription.objects.all()
         self.assertEqual(subscriptions.count(), 1)
@@ -51,45 +54,70 @@ class UserViewSetSubscribeTest(
             subscriber=subscriber,
         )
 
-    def test_unsuccessful_subscribe_with_status_400(self) -> None:
-        """Проверка неудачного оформления подписки со статусом 400 (ошибка подписки)."""
+    def test_user_cannot_subscribe_twice(self) -> None:
+        """Проверка, что пользователь не может повторно подписаться на одного и того же человека"""
 
         login_user(self.client, self.user)
 
         response = self.client.get(
-            reverse('api:users-subscribe', args=[self.author.pk]),
+            reverse(self.base_url, args=[self.author.pk]),
         )
         self.assert_status_equal(response, status.HTTP_201_CREATED)
 
         response = self.client.get(
-            reverse('api:users-subscribe', args=[self.author.pk]),
+            reverse(self.base_url, args=[self.author.pk]),
         )
         self.assert_status_equal(response, status.HTTP_400_BAD_REQUEST)
 
         self.assertEqual(Subscription.objects.count(), 1)
 
-    def test_unsuccessful_subscribe_with_status_401(self) -> None:
-        """Проверка неудачного оформления подписки со статусом 401 (пользователь не авторизован)."""
+    def test_unauthenticated_user_cannot_subscribe_on_other_user(self) -> None:
+        """Проверка, что неавторизованный пользователь не может оформить подписку"""
 
         response = self.client.get(
-            reverse('api:users-subscribe', args=[self.author.pk]),
+            reverse(self.base_url, args=[self.author.pk]),
         )
         self.assert_status_equal(response, status.HTTP_401_UNAUTHORIZED)
 
         self.assertEqual(Subscription.objects.count(), 0)
 
-    def test_unsuccessful_subscribe_with_status_404(self) -> None:
-        """Проверка неудачного оформления подписки со статусом 404 (объект не найден)."""
+    def test_user_cannot_subscribe_on_non_existent_user(self) -> None:
+        """Проверка, что пользователь не может подписаться на несуществующего пользователя"""
 
         login_user(self.client, self.user)
 
         incorrect_user_id = 999
         response = self.client.get(
-            reverse('api:users-subscribe', args=[incorrect_user_id]),
+            reverse(self.base_url, args=[incorrect_user_id]),
         )
         self.assert_status_equal(response, status.HTTP_404_NOT_FOUND)
 
         self.assertEqual(Subscription.objects.count(), 0)
-        self.assertFalse(
-            CustomUser.objects.filter(pk=incorrect_user_id).exists(),
+        self.assert_instance_does_not_exist(
+            CustomUser,
+            pk=incorrect_user_id,
+        )
+
+    def test_user_can_unsubscribe_from_user(self) -> None:
+        """Проверка успешного удаления подписки"""
+
+        login_user(self.client, self.user)
+
+        subscription = SubscriptionFactory.create(
+            subscriber=self.user,
+            author=self.author,
+        )
+        self.assert_instance_exists(
+            Subscription,
+            pk=subscription.pk,
+        )
+
+        response = self.client.delete(
+            reverse(self.base_url, args=[self.author.pk]),
+        )
+
+        self.assert_status_equal(response, status.HTTP_204_NO_CONTENT)
+        self.assert_instance_does_not_exist(
+            Subscription,
+            pk=subscription.pk,
         )
