ad158b281f28631801abe9f3704993acd6c0b215
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-02 19:23:06 +0300

    Change djoser settings

--- src/backend/config/settings/components/common.py
+++ src/backend/config/settings/components/common.py
@@ -177,7 +177,10 @@ DJOSER = {
     'SERIALIZERS': {
         'user': 'apps.users.api.serializers.UserSerializer',
         'user_create': 'apps.users.api.serializers.CustomUserCreateSerializer',
-        'current_user': 'apps.users.api.serializers.UserSerializer',
+        'current_user': 'apps.users.api.serializers.CurrentUserSerializer',
+    },
+    'PERMISSIONS': {
+        'user': ['rest_framework.permissions.IsAuthenticated'],
     },
     'HIDE_USERS': False,
     'LOGOUT_ON_PASSWORD_CHANGE': True,
