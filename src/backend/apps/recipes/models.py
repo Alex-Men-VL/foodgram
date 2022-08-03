01d04a2682188165b619afc3de515f6c88a32998
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-03 23:04:45 +0300

    Fix RecipeIngredient str method

--- src/backend/apps/recipes/models.py
+++ src/backend/apps/recipes/models.py
@@ -92,4 +92,4 @@ class RecipeIngredient(models.Model):
         )
 
     def __str__(self) -> str:
-        return f'{self.recipe}: {self.ingredient.name} {self.quantity}'.strip()
+        return f'{self.recipe}: {self.ingredient.name} {self.amount}'.strip()
