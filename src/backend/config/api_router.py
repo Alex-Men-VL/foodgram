319fe62123c98cf611399780e2f2ff34324d5fb1
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-04 22:52:25 +0300

    Sort imports

--- src/backend/config/api_router.py
+++ src/backend/config/api_router.py
@@ -4,9 +4,9 @@ from django.urls import include
 from django.urls import path
 
 from apps.ingredients.api.views import IngredientViewSet
+from apps.recipes.api.views import RecipeViewSet
 from apps.tags.api.views import TagViewSet
 from apps.users.api.views import UserViewSet
-from apps.recipes.api.views import RecipeViewSet
 
 app_name = 'api'
 
