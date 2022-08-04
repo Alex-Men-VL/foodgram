a8073fc08ef3af60b573b69c3270fc217e37bbf6
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-04 14:08:21 +0300

    Add RecipeIngredient Serializer

--- src/backend/apps/recipes/api/serializers/base_serializers.py
+++ src/backend/apps/recipes/api/serializers/base_serializers.py
@@ -1,6 +1,11 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 
+from apps.ingredients.api.serializers import IngredientSerializer
+
 from ...models import Recipe
+from ...models import RecipeIngredient
 
 
 class ShortRecipeSerializer(serializers.ModelSerializer):
@@ -16,3 +21,25 @@ class ShortRecipeSerializer(serializers.ModelSerializer):
             'image',
             'cooking_time',
         )
+
+
+class RecipeIngredientFlatSerializer(serializers.ModelSerializer):
+    """Сериализатор ингредиента в рецепте"""
+
+    ingredient = IngredientSerializer()
+
+    class Meta:
+        model = RecipeIngredient
+        fields = (
+            'ingredient',
+            'amount',
+        )
+
+    def to_representation(self, instance: RecipeIngredient) -> OrderedDict:
+        representation = super().to_representation(instance)
+
+        ingredient_representation = representation.pop('ingredient')
+        for key in ingredient_representation:
+            representation[key] = ingredient_representation[key]
+
+        return representation
