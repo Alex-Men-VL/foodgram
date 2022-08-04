b7d8cac46e4557578d7e80a13a575b29cbe50cd9
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-04 14:10:05 +0300

    Add recipes api endpoint

--- src/backend/config/api_router.py
+++ src/backend/config/api_router.py
@@ -6,6 +6,7 @@ from django.urls import path
 from apps.ingredients.api.views import IngredientViewSet
 from apps.tags.api.views import TagViewSet
 from apps.users.api.views import UserViewSet
+from apps.recipes.api.views import RecipeViewSet
 
 app_name = 'api'
 
@@ -14,6 +15,7 @@ router = SimpleRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
