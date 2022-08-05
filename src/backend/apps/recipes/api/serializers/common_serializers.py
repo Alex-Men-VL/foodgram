1864788967c5f11308f0b87c41df2e116b232dd6
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-06 00:12:10 +0300

    Replace recipe update logic in mixin

--- src/backend/apps/recipes/api/serializers/common_serializers.py
+++ src/backend/apps/recipes/api/serializers/common_serializers.py
@@ -1,5 +1,3 @@
-import typing
-
 from drf_extra_fields.fields import Base64ImageField
 from drf_writable_nested import NestedCreateMixin
 from rest_framework import serializers
@@ -13,7 +11,7 @@ from . import RecipeIngredientFlatCreateSerializer
 from . import RecipeIngredientFlatRetrieveSerializer
 from . import ShortRecipeSerializer
 from ...models import Recipe
-from ...selectors import get_recipe_ingredients
+from ..mixins import RecipeUpdateMixin
 
 
 class RecipeRetrieveSerializer(ShortRecipeSerializer):
@@ -73,7 +71,11 @@ class RecipeRetrieveSerializer(ShortRecipeSerializer):
         return getattr(obj, 'is_in_shopping_cart')
 
 
-class RecipeCreateSerializer(NestedCreateMixin, ShortRecipeSerializer):
+class RecipeCreateSerializer(
+    NestedCreateMixin,
+    RecipeUpdateMixin,
+    ShortRecipeSerializer,
+):
     """Сериализатор создания рецепта"""
 
     image = Base64ImageField()
@@ -97,69 +99,3 @@ class RecipeCreateSerializer(NestedCreateMixin, ShortRecipeSerializer):
             'text',
         )
         fields = ShortRecipeSerializer.Meta.fields + additional_fields  # type: ignore
-
-    def update(
-        self,
-        instance: Recipe,
-        validated_data: typing.Dict,
-    ) -> Recipe:
-        """Обновление рецепта"""
-
-        ingredients_relations = self._extract_ingredients_relations(validated_data)
-
-        # Update instance
-        instance = super().update(
-            instance,
-            validated_data,
-        )
-
-        self.delete_recipe_ingredients(instance)
-        if ingredients_relations:
-            self.create_recipe_ingredients(instance, ingredients_relations)
-
-        instance.refresh_from_db()
-        return instance
-
-    def _extract_ingredients_relations(
-        self,
-        validated_data: typing.Dict,
-    ) -> typing.Union[typing.List[typing.Dict[str, int]] | None]:
-        """Получение ингредиентов рецепта"""
-
-        ingredients_field_name = 'ingredients'
-        if ingredients_field := self.fields.get(ingredients_field_name):
-            validated_data.pop(ingredients_field.source)
-
-        return self.get_initial().get(ingredients_field_name)
-
-    def delete_recipe_ingredients(
-        self,
-        recipe: Recipe,
-    ) -> None:
-        """Удаление рецептов
-
-        :param recipe: Текущий рецепт
-        """
-
-        recipe_ingredients = get_recipe_ingredients(recipe)
-        recipe_ingredients.delete()
-
-    def create_recipe_ingredients(
-        self,
-        recipe: Recipe,
-        ingredients_relations: typing.List[typing.Dict[str, int]],
-    ) -> None:
-        """Создание ингредиентов для текущего рецепта
-
-        :param recipe: Текущий рецепт
-        :param ingredients_relations: Ингредиенты
-        """
-
-        for ingredient in ingredients_relations:
-            serializer = RecipeIngredientFlatCreateSerializer(
-                data=ingredient,
-            )
-            serializer.is_valid(raise_exception=True)
-            serializer.save(
-                recipe=recipe,
-            )
