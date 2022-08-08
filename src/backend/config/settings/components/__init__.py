282a54ea2f9738cacf063d8f5b5e6f97c1a61916
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-08 13:08:13 +0300

    Replace split .env files with one common

--- src/backend/config/settings/components/__init__.py
+++ src/backend/config/settings/components/__init__.py
@@ -1,14 +1,10 @@
-from os import environ
 from pathlib import Path
 
 from decouple import AutoConfig
 
 BASE_DIR = Path(__file__).parent.parent.parent.parent
 
-# Managing environment via `DJANGO_ENV` variable:
-environ.setdefault('DJANGO_ENV', 'development')
-_ENV = environ['DJANGO_ENV']
 
 config = AutoConfig(
-    search_path=BASE_DIR.joinpath(f'.envs/{_ENV}'),
+    search_path=BASE_DIR.joinpath('.envs'),
 )
