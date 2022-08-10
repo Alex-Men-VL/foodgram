17634ecf5f0ddb35911a587c9a8dc87cadd443a4
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-10 21:55:13 +0300

    Add Ingredient constraints

--- src/backend/apps/ingredients/models.py
+++ src/backend/apps/ingredients/models.py
@@ -3,6 +3,9 @@ import uuid
 from behaviors import behaviors
 
 from django.db import models
+from django.db.models import functions
+
+models.CharField.register_lookup(functions.Length, 'len')
 
 
 class Ingredient(behaviors.Timestamped):
@@ -27,9 +30,22 @@ class Ingredient(behaviors.Timestamped):
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
         ordering = ('name',)
-        unique_together = (
-            'name',
-            'measurement_unit',
+        constraints = (
+            models.UniqueConstraint(
+                fields=(
+                    'name',
+                    'measurement_unit',
+                ),
+                name='%(app_label)s_%(class)s_name_measurement_unit_unique_together',
+            ),
+            models.CheckConstraint(
+                check=models.Q(name__len__gt=0),
+                name='%(app_label)s_%(class)s_name_is_empty',
+            ),
+            models.CheckConstraint(
+                check=models.Q(measurement_unit__len__gt=0),
+                name='%(app_label)s_%(class)s_measurement_unit_is_empty',
+            ),
         )
 
     def __str__(self) -> str:
