08ed4273dd283c9374e11617b5c2b1d9e05c7cd4
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-16 21:23:01 +0300

    Update deployment script

--- kubernetes/prod/deploy.sh
+++ kubernetes/prod/deploy.sh
@@ -5,7 +5,23 @@ cd `dirname "$0"`
 echo "Run deploy"
 echo
 
+# Setup configmap
 kubectl apply -f configmap.yaml
-kubectl apply -f component_backend.yaml
-kubectl apply -f component_frontend.yaml
+
+# Run django jobs
+for manifest in job_django.yaml cron_job_clearsessions.yaml
+do
+    kubectl apply -f $manifest
+done
+
+# Start backend & frontend services
+for manifest in component_django.yaml component_react.yaml
+do
+    kubectl apply -f $manifest
+done
+
+# Start ingress service
 kubectl apply -f ingress_service.yaml
+
+echo
+echo "Project deployed successfully"
