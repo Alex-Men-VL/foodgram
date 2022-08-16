ab81e7ef3f56603ec0a63329cf8e4fe804fa67ac
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-08-16 21:20:19 +0300

    Move migrate & collectstatic commands from gunicorn.sh to entrypoint.sh

--- src/backend/docker/django/gunicorn.sh
+++ src/backend/docker/django/gunicorn.sh
@@ -8,9 +8,6 @@ set -o pipefail
 # Run python specific scripts:
 # Running migrations in startup script might not be the best option, see:
 # docs/pages/template/production-checklist.rst
-python ./manage.py migrate --noinput
-python ./manage.py collectstatic --noinput
-#python ./manage.py compilemessages
 
 # Start gunicorn:
 # Docs: http://docs.gunicorn.org/en/stable/settings.html
